669c2f6fafb1250880e01ca492dc6de95aa7e500
Piyush0450 <140835828+Piyush0450@users.noreply.github.com>
2025-10-01 22:50:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -235,7 +235,8 @@ def main():
     application.add_error_handler(error_callback)
     
     logger.info("Bot started successfully!")
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+    application.run_polling()
+
 
 if __name__ == '__main__':
     main()
